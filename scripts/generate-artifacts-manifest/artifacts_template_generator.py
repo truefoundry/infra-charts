c207833573dd5d8d5b162a58fe977c81c9ca457b
Dipo Ajayi <ajayidipo@ymail.com>
2024-10-31 20:44:40 +0100

    remove ecr auth

--- scripts/generate-artifacts-manifest/artifacts_template_generator.py
+++ scripts/generate-artifacts-manifest/artifacts_template_generator.py
@@ -134,7 +134,10 @@ def get_image_details(image_name):
     Returns:
         dict: A JSON-formatted dictionary containing the image type, details, and supported platforms.
     """
-
+    # skip if image registry starts with 'ecr.public'
+    if image_name.startswith('602401143452.dkr.ecr') or image_name.startswith('auto'):
+        print(f"Skipping image {image_name} as it is an Auth image")
+        return []
     # Run 'docker manifest inspect' command
     try:
         result = run_command(f'docker manifest inspect {image_name}')
