d2ecf3c4164069ef57595c21e971e00dce39de3c
Dipo Ajayi <ajayidipo@ymail.com>
2024-12-23 09:29:00 +0100

    add truefoundry charts

--- scripts/generate-artifacts-manifest/check_chart_images_support_multiple_architectures.py
+++ scripts/generate-artifacts-manifest/check_chart_images_support_multiple_architectures.py
@@ -7,6 +7,7 @@ from pathlib import Path
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+truefoundry_charts = ["truefoundry", "tfy-notebook-controller", "tfy-agent", "elasti"]
 
 def load_json_file(file_path: str):
     """Load and return the contents of a JSON file."""
@@ -44,7 +45,7 @@ def parse_args(args):
 def get_truefoundry_chart_images(artifacts_manifest):
     """Extract the list of images associated with the 'truefoundry' chart."""
     for artifact in artifacts_manifest:
-        if artifact.get("details", {}).get("chart") == "truefoundry":
+        if artifact.get("details", {}).get("chart") in truefoundry_charts:
             return artifact["details"].get("images", [])
     return []
 
