9fd241d2b65272e700e4f9ce3bcce88e4a9cd26f
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2025-01-07 14:45:12 +0530

    Backend auth modified (#918)

--- scripts/infra-automation/pre-requisites.sh
+++ scripts/infra-automation/pre-requisites.sh
@@ -222,20 +222,20 @@ function create_backend() {
 
 		local bucket_name=$(echo $manifest_inputs | jq -r '.manifest.backend.bucket_name')
 		local region=$(echo $manifest_inputs | jq -r '.manifest.region')
-		local auth_type=$(echo $manifest_inputs | jq -r '.manifest.backend.auth.type')
+		local auth_type=$(echo $manifest_inputs | jq -r '.manifest.auth.type')
 		local dynamodb_table=$(echo $manifest_inputs | jq -r '.manifest.backend.dynamodb_table')
 
 		
 		if [[ $auth_type == "profile" ]]
 		then
 			log_debug "Auth mode $auth_type"
-			local profile=$(echo $manifest_inputs | jq -r '.manifest.backend.auth.profile')
+			local profile=$(echo $manifest_inputs | jq -r '.manifest.auth.profile')
 			export AWS_PROFILE=$profile
 		else
-			local access_key=$(echo $manifest_inputs | jq -r '.manifest.backend.auth.access_key')
+			local access_key=$(echo $manifest_inputs | jq -r '.manifest.auth.access_key')
 			export AWS_ACCESS_KEY_ID=$access_key
 			export AWS_SECRET_ACCESS_KEY=$secret_key
-			local secret_key=$(echo $manifest_inputs | jq -r '.manifest.backend.auth.secret_key')
+			local secret_key=$(echo $manifest_inputs | jq -r '.manifest.auth.secret_key')
 		fi
 		
 
