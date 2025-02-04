337cf805743e2e9fd9743b57a5803f30c926b819
Gowtham Gorrepati <gowtham@truefoundry.com>
2025-02-04 15:38:41 +0530

    add s3 bucket name

--- scripts/publish-app-catalogue/sync-manifests-to-s3.sh
+++ scripts/publish-app-catalogue/sync-manifests-to-s3.sh
@@ -2,13 +2,14 @@
 set -e
 
 # validate arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <inframold_name> <cloud_provider>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <inframold_name> <cloud_provider> <aws_s3_bucket>"
     exit 1
 fi
 
 INFRAMOLD_NAME=$1
 CLOUD_PROVIDER=$2
+AWS_S3_BUCKET=$3
 
 copy_to_s3() {
     local directory=$1
@@ -22,7 +23,7 @@ copy_to_s3() {
                 exit 1
             fi
             # Copy the addon file to S3
-            aws s3 cp "${file}" "s3://tfy-argo-application-catalogue/${cloud_provider}/addons/${addon_name}/${addon_target_revision}.yaml"
+            aws s3 cp "${file}" "s3://${AWS_S3_BUCKET}/${cloud_provider}/addons/${addon_name}/${addon_target_revision}.yaml"
         elif [ -d "${file}" ]; then # if folder, recursively call copy_to_s3
             copy_to_s3 "${file}" "${cloud_provider}"
         fi
@@ -41,10 +42,10 @@ helm template inframold -n argocd -f "./charts/${INFRAMOLD_NAME}/values-helm.yam
 cp_chart_version=$(yq e '.spec.source.targetRevision' "./catalogues/${INFRAMOLD_NAME}/templates/truefoundry.yaml")
 
 # Sync to S3
-aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://tfy-argo-application-catalogue/${CLOUD_PROVIDER}/templates" --delete
+aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLOUD_PROVIDER}/templates" --delete
 
 # Sync to S3 chart version folder
-aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://tfy-argo-application-catalogue/${CLOUD_PROVIDER}/${cp_chart_version}/templates" --delete
+aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLOUD_PROVIDER}/${cp_chart_version}/templates" --delete
 
 # Copy to S3 addon version files
 copy_to_s3 "./catalogues/${INFRAMOLD_NAME}/templates" "${CLOUD_PROVIDER}"
