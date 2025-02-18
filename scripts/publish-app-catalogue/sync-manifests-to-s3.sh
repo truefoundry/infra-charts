14d13b529c27fa562935cc142159fe3e3acb29b0
Gowtham Gorrepati <gowtham@truefoundry.com>
2025-02-18 21:58:46 +0530

    chore: update addon s3 sync

--- scripts/publish-app-catalogue/sync-manifests-to-s3.sh
+++ scripts/publish-app-catalogue/sync-manifests-to-s3.sh
@@ -3,37 +3,56 @@ set -e
 
 # validate arguments
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <inframold_name> <cloud_provider> <aws_s3_bucket>"
+    echo "Usage: $0 <inframold_name> <cluster_type> <aws_s3_bucket>"
     exit 1
 fi
 
 INFRAMOLD_NAME=$1
-CLOUD_PROVIDER=$2
+CLUSTER_TYPE=$2
 AWS_S3_BUCKET=$3
 
-copy_to_s3() {
+validate_target_revision() {
+    local addon_target_revision=$1
+    if [ -z "${addon_target_revision}" ] || [ "${addon_target_revision}" == "null" ]; then
+        echo "Addon target revision is null or not defined. Exiting."
+        exit 1
+    fi
+}
+
+copy_addon_manifest_to_s3() {
     local directory=$1
-    local cloud_provider=$2
-    for file in "${directory}"/*; do
-        if [ -f "${file}" ]; then
-            addon_name=$(basename "${file}" .yaml)
-            addon_target_revision=$(yq e '.spec.source.targetRevision' "${file}")
-            if [ -z "${addon_target_revision}" ] || [ "${addon_target_revision}" == "null" ]; then
-                echo "Addon target revision is null or not defined. Exiting."
-                exit 1
-            fi
-            # Copy the addon file to S3
-            aws s3 cp "${file}" "s3://${AWS_S3_BUCKET}/${cloud_provider}/addons/${addon_name}/${addon_target_revision}.yaml"
-        elif [ -d "${file}" ]; then # if folder, recursively call copy_to_s3
-            copy_to_s3 "${file}" "${cloud_provider}"
-        fi
+    local cluster_type=$2
+    for file in $(find "${directory}" -type f); do
+        addon_name=$(basename "${file}" .yaml)
+        addon_target_revision=$(yq e '.spec.source.targetRevision' "${file}")
+        validate_target_revision "${addon_target_revision}"
+        # Copy the addon file to S3
+        aws s3 cp "${file}" "s3://${AWS_S3_BUCKET}/addons/${cluster_type}/addons/${addon_name}/versions/${addon_target_revision}/manifest.yaml"
     done
 }
 
-echo "Rendering ${CLOUD_PROVIDER} k8s manifests..."
+copy_control_plane_addon_version_to_s3() {
+    local directory=$1
+    local cluster_type=$2
+    local cp_chart_version=$3
+    local target_addon_version_file_path="/tmp/cp_addon_versions/targetAddonVersion.yaml"
+    mkdir -p /tmp/cp_addon_versions
+    touch ${target_addon_version_file_path}
+    for file in $(find "${directory}" -type f); do
+        addon_name=$(basename "${file}" .yaml)
+        addon_target_revision=$(yq e '.spec.source.targetRevision' "${file}")
+        validate_target_revision "${addon_target_revision}"
+        yq e ".${addon_name} = ${addon_target_revision}" -i ${target_addon_version_file_path}
+    done
+    # Copy the target version map to S3
+    aws s3 cp "${target_addon_version_file_path}" "s3://${AWS_S3_BUCKET}/addons/${cluster_type}/control-planes/${cp_chart_version}/targetAddonVersion.yaml"
+    rm -rf /tmp/cp_addon_versions
+}
+
+echo "Rendering ${CLUSTER_TYPE} k8s manifests..."
 
 additional_options=""
-if [ "${CLOUD_PROVIDER}" = "aws-eks" ]; then
+if [ "${CLUSTER_TYPE}" = "aws-eks" ]; then
     additional_options="--set aws.karpenter.defaultZones={\"\"}"
 fi
 helm template inframold -n argocd -f "./charts/${INFRAMOLD_NAME}/values-helm.yaml" -f "./charts/${INFRAMOLD_NAME}/values-ocli.yaml" "./charts/${INFRAMOLD_NAME}" ${additional_options} --output-dir catalogues
@@ -45,15 +64,18 @@ cp_chart_version=$(yq e '.spec.source.targetRevision' "./catalogues/${INFRAMOLD_
 cp_preview_version="preview"
 
 # Sync to S3
-aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLOUD_PROVIDER}/templates" --delete
+aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLUSTER_TYPE}/templates" --delete
 
 # Sync to S3 chart version folder
-aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLOUD_PROVIDER}/${cp_chart_version}/templates" --delete
+aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLUSTER_TYPE}/${cp_chart_version}/templates" --delete
 
 # Sync to S3 preview version folder
-aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLOUD_PROVIDER}/${cp_preview_version}/templates" --delete
+aws s3 sync "./catalogues/${INFRAMOLD_NAME}/templates" "s3://${AWS_S3_BUCKET}/${CLUSTER_TYPE}/${cp_preview_version}/templates" --delete
 
-# Copy to S3 addon version files
-copy_to_s3 "./catalogues/${INFRAMOLD_NAME}/templates" "${CLOUD_PROVIDER}"
+# Copy addon version manifests to S3
+copy_addon_manifest_to_s3 "./catalogues/${INFRAMOLD_NAME}/templates" "${CLUSTER_TYPE}"
 
-echo "Synced catalogue for ${CLOUD_PROVIDER} successfully."
+# Copy control plane addon version map to S3
+copy_control_plane_addon_version_to_s3 "./catalogues/${INFRAMOLD_NAME}/templates" "${CLUSTER_TYPE}" "${cp_chart_version}"
+
+echo "Synced catalogue for ${CLUSTER_TYPE} successfully."
