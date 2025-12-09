6dc86e14f07062c9745eeba9c5af4e21429ebc50
truefoundry-saas <truefoundry-saas@users.noreply.github.com>
2025-12-09 10:49:15 +0000

    [CI] Publish Truefoundry chart version 0.106.1

--- charts/truefoundry/tfy-build-scripts/build-soci-index.sh
+++ charts/truefoundry/tfy-build-scripts/build-soci-index.sh
@@ -6,24 +6,27 @@ echo "Building and pushing SOCI index"
 PASSWORD=$(echo "$DOCKER_REGISTRY_PASSWORD" | base64 -d)
 IMAGE=$DOCKER_REGISTRY_URL/$DOCKER_REPO:$DOCKER_TAG
 
-
 echo Starting Containerd
 containerd > /dev/null 2>&1 &
 sleep 3
 ctr version > /dev/null
 
-echo Pulling Container Image "$IMAGE"
-
-time ctr content fetch -u "$DOCKER_REGISTRY_USERNAME:$PASSWORD" \
---platform=linux/amd64 \
-"$IMAGE" > /dev/null
-
-
-echo Creating Soci Index
-
-time soci create --platform=linux/amd64 "$IMAGE"
-
-echo Pushing Soci Index
-time soci push --platform=linux/amd64 "$IMAGE"
+echo "Logging into registry: ${DOCKER_REGISTRY_URL}"
+nerdctl login -u "$DOCKER_REGISTRY_USERNAME" -p "$PASSWORD" "$DOCKER_REGISTRY_URL" 2>&1 || { echo "Error: nerdctl login failed, make sure your authentication credentials are correct in registry!" >&2; exit 1; }
+echo "Pulling image ${IMAGE}"
+time nerdctl pull --unpack false --quiet --platform linux/amd64 "${IMAGE}"
+echo "Staging ${IMAGE} for soci conversion for platform: linux/amd64"
+nerdctl tag "${IMAGE}" "${IMAGE}-temp"
+nerdctl rmi "${IMAGE}"
+echo "Creating soci index for ${IMAGE} for platform: linux/amd64"
+time soci convert --platform linux/amd64 "${IMAGE}-temp" "${IMAGE}"
+echo "Pushing soci index for ${IMAGE} for platform: linux/amd64"
+time nerdctl push --quiet --platform linux/amd64 "${IMAGE}"
+echo "Removing soci index for ${IMAGE}"
+soci index rm --ref "${IMAGE}"
+echo "Removing image for ${IMAGE}"
+nerdctl rmi "${IMAGE}-temp" "${IMAGE}"
+echo "Rebuilding soci database"
+soci rebuild-db
 
 echo Done!
